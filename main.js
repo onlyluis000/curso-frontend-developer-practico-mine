0f00f533d3fe57beda30f39bdffdda9ca84d7ba5
onlyluis000 <luis.alberto.rodriguez.torres117@gmail.com>
2022-12-13 08:34:56 -0600

    creamos el index y juntamos clase 7 con 11, creamos el readme, main js y styles y empezamos a trabajar con ellos

---
+++ main.js
@@ -0,0 +1,7 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+menuEmail.addEventListener('click', toggleMenu);
+
+function toggleMenu (){
+desktopMenu.classList.toggle('toggleShowing');
+}
