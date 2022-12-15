55a736ccfa29a59c58cf6e613f82b8f967c9fb96
onlyluis000 <luis.alberto.rodriguez.torres117@gmail.com>
2022-12-15 08:48:35 -0600

    integré menú mobile al index con media queries y mostrar/ocultar correspondientes

--- main.js
+++ main.js
@@ -2,6 +2,21 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 menuEmail.addEventListener('click', toggleMenu);
 
-function toggleMenu (){
-desktopMenu.classList.toggle('toggleShowing');
+const menuMobile = document.querySelector('.mobile-menu');
+const menu = document.querySelector('.menu');
+menu.addEventListener('click', toggleMenu);
+
+
+function toggleMenu (event){
+    console.log(event)
+    switch(event.target.className){
+    case 'menu':
+        menuMobile.classList.toggle('toggleShowing');
+    break;
+    
+    case 'navbar-email':
+    desktopMenu.classList.toggle('toggleShowing');
+    break;
+    default: break;
+    }
 }
